7913f34b8458539c452e84ec8a8e7e3c2c22cd07
AdityaNair07 <nairaditya07@gmail.com>
2024-06-11 15:50:00 +0530

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import '@/app/ui/global.css';
+import { roboto_slab } from '@/app/ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${roboto_slab.className} antialiased`}>{children}</body>
     </html>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Roboto_Slab, Lusitana } from 'next/font/google';
+
+export const roboto_slab = Roboto_Slab({ subsets: ['latin'] });
+export const lusitana = Lusitana({ weight: ['400'], subsets: ['latin'] });
